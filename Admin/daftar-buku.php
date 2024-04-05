219320cd73adc92cc44ed5b1e97db133b79008ef
Gafisk <galihrestubaihq@gmail.com>
2024-04-06 01:31:50 +0700

    Update Kedua

--- Admin/daftar-buku.php
+++ Admin/daftar-buku.php
@@ -33,41 +33,7 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="#" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
-        <span class="brand-text font-weight-light">Administrator</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" />
-          </div>
-          <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
-          </div>
-        </div>
-
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-          <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
-            <div class="input-group-append">
-              <button class="btn btn-sidebar">
-                <i class="fas fa-search fa-fw"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <?php include('layouts/navbar.php'); ?>
-      </div>
-      <!-- /.sidebar -->
-    </aside>
+    <?php include('layouts/main-user.php'); ?>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -91,44 +57,44 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-lg-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Data Buku Perpustakaan SMK Negeri 2 Bangkalan</h3>
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Data Buku Perpustakaan SMK Negeri 2 Bangkalan</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>Kategori</th>
+                        <th>Kelas</th>
+                        <th>Judul</th>
+                        <th>Pengarang</th>
+                        <th>Tahun Terbit</th>
+                        <th>Penerbit</th>
+                        <th>Jumlah</th>
+                        <th>Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Pendidikan</td>
+                        <td>X</td>
+                        <td>Belajar Grafika Komputer dengan JAVA</td>
+                        <td>Galih Restu Baihaqi</td>
+                        <td>2022</td>
+                        <td>Wade Group</td>
+                        <td>12</td>
+                        <td>
+                          <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                          <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                        </td>
+                      </tr>
+                      </tfoot>
+                  </table>
+                </div>
+                <!-- /.card-body -->
               </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                    <th>Kategori</th>
-                    <th>Kelas</th>
-                    <th>Judul</th>
-                    <th>Pengarang</th>
-                    <th>Tahun Terbit</th>
-                    <th>Penerbit</th>
-                    <th>Jumlah</th>
-                    <th>Aksi</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>Pendidikan</td>
-                    <td>X</td>
-                    <td>Belajar Grafika Komputer dengan JAVA</td>
-                    <td>Galih Restu Baihaqi</td>
-                    <td>2022</td>
-                    <td>Wade Group</td>
-                    <td>12</td>
-                    <td>
-                      <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                      <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                    </td>
-                  </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
             </div>
           </div>
         </div>
@@ -136,26 +102,8 @@
       </section>
       <!-- /.content -->
     </div>
-    <!-- /.content-wrapper -->
-    <footer class="main-footer">
-      <strong>Copyright &copy; 2014-2021
-        <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-      All rights reserved.
-      <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
-      </div>
-    </footer>
 
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-  </div>
-  <!-- ./wrapper -->
-
-<?php include('layouts/footer.php'); ?>
-</script>
+    <?php include('layouts/footer.php'); ?>
 </body>
 
 </html>
